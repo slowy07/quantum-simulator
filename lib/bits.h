8faf5f633feabad5175b34ccce892e75999fd6d8
arfy slowy <slowy.arfy@gmail.com>
2022-06-07 03:13:31 -0300

    update libs
    Signed-off-by: arfy slowy <slowy.arfy@gmail.com>

--- lib/bits.h
+++ lib/bits.h
@@ -18,6 +18,33 @@ namespace clfsim {
         inline uint32_t ExpandBits(uint64_t bits, unsigned n, uint32_t mask) {
             return _pdep_u64(bits, mask);
         }
+        
+        inline uint32_t CompressBits(uint32_t bits, unsigned n, uint32_t mask) {
+            return _pext_u32(bits, mask);
+        }
+
+        inline uint64_t CompressBits(uint64_t bits, unsigned n, uint64_t mask) {
+            return _pext_u64(bits, mask);
+        }
+    }
+}
+
+#else
+
+namespace clfsim {
+    namespace bits {
+        template <typename Integer>
+        inline Integer PermuteBits(
+            Integer bits, unsigned n, const std::vector<unsigned>& perm
+        ) {
+            Integer pbits = 0;
+
+            for (unsigned i = 0; i < n; ++i) {
+                pbits |= ((bits >> i) & 1) << perm[i];
+            }
+
+            return pbits
+        }
     }
 }
 
